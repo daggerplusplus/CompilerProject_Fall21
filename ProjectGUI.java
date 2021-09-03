26adfd2e13faf88ac7e0da9bbe26e84e873aa259
daqger <daggerempire@gmail.com>
2021-09-02 22:12:37 -0400

    Co-authored-by: cgraha41 <cgraha41@users.noreply.github.com> Co-authored-by: Kserna03 <Kserna03@users.noreply.github.com> Co-authored-by: live.com#tnbrock@gmail.com <tnbrock@gmail.com>

--- ProjectGUI.java
+++ ProjectGUI.java
@@ -1,26 +1,37 @@
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+//import java.awt.*;
 
-public class ProjectGUI extends JFrame{
+public class ProjectGUI extends JFrame {
     private JPanel mainPanel;
-    private JTextArea originalFile;
-    private JLabel originalTitle;
-    private JLabel translatedFile;
-    private JButton importButton;
-    private JTextArea convertedFile;
-    private JButton saveButton;
-    private JButton translateButton;
+    
+    private JTextArea originalFile; private JLabel originalTitle; private JLabel
+     translatedFile; private JButton importButton; private JTextArea
+      convertedFile; private JButton saveButton; private JButton translateButton;
+     
 
-    public ProjectGUI(String title){
+    public ProjectGUI(String title) {
         super(title);
+        mainPanel = new javax.swing.JPanel();
 
+        JButton b1 = new JButton("hey");
+        mainPanel.add(b1);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
         setSize(700, 700);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFrame frame = new ProjectGUI("Java to C# Translator");
         frame.setVisible(true);
+
+        System.out.println("Your mom");
+        System.out.println("no, you");
+
     }
 }
+    
