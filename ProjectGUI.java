a0f475c23c41a62f1325d1603b97f30ace6a6336
daqger <daggerempire@gmail.com>
2021-09-06 17:23:44 -0400

    updated gui

--- ProjectGUI.java
+++ ProjectGUI.java
@@ -3,37 +3,69 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-//import java.awt.*;
+import javax.swing.JFileChooser;
+import java.awt.LayoutManager;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.io.File;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ProjectGUI extends JFrame {
-    private JPanel mainPanel;
-    
-    private JTextArea originalFile; private JLabel originalTitle; private JLabel
-     translatedFile; private JButton importButton; private JTextArea
-      convertedFile; private JButton saveButton; private JButton translateButton;
-     
+public class ProjectGUI extends JFrame {     
 
-    public ProjectGUI(String title) {
-        super(title);
-        mainPanel = new javax.swing.JPanel();
+    private static void createWindow() {
+        JFrame frame = new JFrame("Java to C# Translator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        createGUI(frame);
+        frame.setSize(700, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
+    private static void createGUI(final JFrame frame) {
+        JPanel mainPanel = new JPanel();
+        LayoutManager layout = new FlowLayout();
+        mainPanel.setLayout(layout);
 
-        //JButton b1 = new JButton("hey");
-        //mainPanel.add(b1);
+        JLabel originalTitle = new JLabel("Java");
+        JLabel translatedFile = new JLabel("C#");
+        JButton importButton = new JButton("Import Java File");
+        JButton saveButton = new JButton("Save C# File");
+        JButton translateButton = new JButton("Translate");
+        JTextArea originalFile = new JTextArea("Enter error-free Java code here",35,25);
+        JTextArea convertedFile = new JTextArea(35,25);
+        
+        mainPanel.add(originalTitle);
+        mainPanel.add(originalFile);
+        mainPanel.add(translatedFile);
+        mainPanel.add(convertedFile);
+        mainPanel.add(importButton);
+        mainPanel.add(saveButton);
+        mainPanel.add(translateButton);
+        frame.getContentPane().add(mainPanel,BorderLayout.CENTER);       
+        
+        
+        
+        importButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int option = fileChooser.showOpenDialog(frame);
+                if (option == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    
+                } else {
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(mainPanel);
-        this.pack();
-        setSize(700, 700);
+                }
+            }
+        
+
+        });
+        
     }
 
     public static void main(String[] args) {
-        JFrame frame = new ProjectGUI("Java to C# Translator");
-        frame.setVisible(true);
-
-        System.out.println("Your mom");
-        System.out.println("no, you");
-
+        createWindow();
     }
 }
     
