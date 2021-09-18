2944078d85459025b35ff52fdae2bfe468368219
Bryan <66394351+daqger@users.noreply.github.com>
2021-09-18 13:26:05 -0400

    Add files via upload

--- input.java
+++ input.java
@@ -1,7 +1,16 @@
 // comment 1
 public abstract class input {
-	// comment 1
+  /*
+  comment 2
+  */
 	public input(String n){
-		// comment 2
-	}
+		System.out.print("YEEEEHAW");
+    for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.println(i+" "+j);
+            }
+        }
+  }
 }
