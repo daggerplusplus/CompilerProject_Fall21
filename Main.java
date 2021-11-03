a9a51a717fa1145d648f167666d20e874f5d4abd
Bryan <66394351+daqger@users.noreply.github.com>
2021-11-02 20:46:53 -0400

    Sprint 3
    Broad upload. Working on interpreter

--- Main.java
+++ Main.java
@@ -1,33 +1,101 @@
-
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import java.awt.LayoutManager;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-  public class Main {
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-
-    public static void main(String[] args) {        
-        ProjectGUI.createWindow();
-        
-        
-
-        /*
-        LexerDraft lexer = new LexerDraft();
-        lexer.readFile();
-        try {
-            lexer.createTokens();
-        } catch (IOException E) {
-
-        }
-        */
+class Main {
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    static List<StmtS2> statements = new ArrayList<>();
+    static ProjectGUIS2 gui = new ProjectGUIS2();
+    
+    public static void main(String[] args) throws IOException {        
+        gui.createWindow();        
     }
+
+    static List<StmtS2> getStmts() {
+        return statements;
+    }
+
+    static void run2() {
+        String path = gui.getInputPath();
+        LexerS2 lexer = new LexerS2(path);        
+        List<TokenS2> tokens = lexer.scanTokens();
+        ParserS2 parser = new ParserS2(tokens);            
+        statements = parser.parse();
+        //private static final Interpreter itptr = new Interpreter();
+        //itptr.interpret(statements);
+        
+        
+        gui.fillList(statements); //how to make this work with interpreter output?
+    }
+
+    static void run(String source) {    
+        gui.createWindow();
+        String path = gui.getInputPath();
+        LexerS2 lexer = new LexerS2(path);        
+        List<TokenS2> tokens = lexer.scanTokens();
+        ParserS2 parser = new ParserS2(tokens);            
+        statements = parser.parse();       
+
+
+/*         for (StmtS2 stmt : statements)
+      {
+        System.out.println(new AstPrinterS2().print(stmt));
+        } */
+
+        // //just print tokens
+        //   for (Token token : tokens) {
+        //   System.out.println(token);
+        // }
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+/*         for(StmtS2 statment : statements ){
+            if(statment != null)
+            {
+//                System.out.println(statment);
+                System.out.println(new AstPrinterS2().print(statment));
+            }
+//            System.out.println(statment);
+//            System.out.println(new AstPrinter().print(statment));
+        } */
+
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+    
+    static void error(TokenS2 token, String message) {
+        if (token.type == TokenTypeS2.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
+   /*  static void runtimeError(RuntimeErrorS2 error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    } */
+
 }
+
+
+
+
+
+
+
